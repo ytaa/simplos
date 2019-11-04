214a0dee06ee6ff5886150d21e0581aaa94ee6d4
Tristan Dobrowolski <trdo1009@gmail.com>
2019-11-04 21:57:02 +0100

    Add parameter error handling to build scripts

--- scripts/load_iso.sh
+++ scripts/load_iso.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
-cd ../kernel/iso
-sudo dd if=$DEVOS_NAME.iso of=$1 && sync
 
+if [ -z $1 ]
+then
+	echo 'ERROR: missing parameter'
+	echo 'Specify target device (probably /dev/sdb1)'
+	echo "Run 'ls /dev/sd*' before and after inserting USB stick to determine device name"
+else
+
+	cd ../kernel/iso
+	sudo dd if=$DEVOS_NAME.iso of=$1 && sync
+
+fi
--- scripts/sim.sh
+++ scripts/sim.sh
@@ -3,7 +3,7 @@ bash build.sh
 cd ../kernel
 if grub-file --is-x86-multiboot bin/$DEVOS_NAME.bin; then 
 	echo MULTIBOOT: OK
-	qemu-system-i386 -kernel bin/$DEVOS_NAME.bin
+	qemu-system-i386 -m 3G -d guest_errors -kernel bin/$DEVOS_NAME.bin
 else 
 	echo MULTIBOOT: FAIL
 	exit 10
